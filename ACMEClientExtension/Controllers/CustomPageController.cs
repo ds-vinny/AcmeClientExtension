822b3215cd7d6071f955867dfe10cb96021c4278
ds-vinny <vinny@directscale.com>
2022-02-16 16:39:48 -0700

    Add a Hello World Custom Page that displays the query string and headers passed to it

---
+++ ACMEClientExtension/Controllers/CustomPageController.cs
@@ -0,0 +1,45 @@
+﻿using IntegrationClientExtension.Models;
+using IntegrationClientExtension.Models.CustomPage;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationClientExtension.Controllers
+{
+    public class CustomPageController : Controller
+    {
+        private readonly ILogger<CustomPageController> _logger;
+
+        public CustomPageController(ILogger<CustomPageController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult HelloWorld([FromQuery]string personsName)
+        {
+            var model = new HelloWorldViewModel();
+            model.PersonsName = personsName;
+            var queryStringKvps = new Dictionary<string, string>();
+            var headersKvps = new Dictionary<string, string>();
+
+            foreach (var queryKvp in HttpContext.Request.Query.Keys)
+            {
+                queryStringKvps.Add(queryKvp, HttpContext.Request.Query[queryKvp]);
+            }
+            model.QueryStringParameters = queryStringKvps;
+
+            foreach (var headerKvp in HttpContext.Request.Headers)
+            {
+                headersKvps.Add(headerKvp.Key, headerKvp.Value.ToString());
+            }
+            model.Headers = headersKvps;
+
+            return View(model);
+        }
+    }
+}
