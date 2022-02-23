b4cd0a076601b03dbe97375a5b6f0bbf4dcd5791
ds-vinny <vinny@directscale.com>
2022-02-23 16:00:08 -0700

    Implement a After Hook with a Controller

--- ACMEClientExtension/Controllers/Hooks/AutoshipHooksController.cs
+++ ACMEClientExtension/Controllers/Hooks/AutoshipHooksController.cs
@@ -31,5 +31,25 @@ namespace ACMEClientExtension.Controllers.Hooks
 
             return Ok(response);
         }
+
+        public async Task<ActionResult<GetAutoshipsHookResponse>> GetAutoshipsHook([FromBody] GetAutoshipsHookRequest request, [FromHeader] string callbackToken)
+        {
+            // This is an Example of an After Hook
+            // Execute the DirectScale callback method first
+            GetAutoshipsHookResponse result = await _hookCallback.CallBack<GetAutoshipsHookRequest, GetAutoshipsHookResponse>(callbackToken, request);
+
+            // Then update the result of the DirectScale Callback method so that the Address is in ALL CAPS
+            foreach (var autoship in result.Autoships)
+            {
+                autoship.ShipAddress.AddressLine1 = autoship.ShipAddress.AddressLine1.ToUpper();
+                autoship.ShipAddress.AddressLine2 = autoship.ShipAddress.AddressLine2.ToUpper();
+                autoship.ShipAddress.City = autoship.ShipAddress.City.ToUpper();
+                autoship.ShipAddress.CountryCode = autoship.ShipAddress.CountryCode.ToUpper();
+                autoship.ShipAddress.State = autoship.ShipAddress.State.ToUpper();
+            }
+
+            // Return the modified result
+            return Ok(result);
+        }
     }
 }
