f752b0b4886bf7bec4a4592ddd80818a55893ad7
ds-vinny <vinny@directscale.com>
2022-02-25 13:24:39 -0700

    Implement a Before Hook with a Controller

---
+++ ACMEClientExtension/Controllers/Hooks/AutoshipHooksController.cs
@@ -0,0 +1,37 @@
+﻿using DirectScale.Disco.Extension.Hooks.Autoships;
+using DirectScale.Disco.Extension.Middleware;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ACMEClientExtension.Controllers.Hooks
+{
+    [ExtensionAuthorize]
+    [Route("api/hooks/[controller]")]
+    [ApiController]
+    public class AutoshipHooksController : ControllerBase
+    {
+        private readonly IHookCallback _hookCallback;
+        public AutoshipHooksController(IHookCallback hookCallback)
+        {
+            _hookCallback = hookCallback ?? throw new ArgumentNullException(nameof(hookCallback));
+        }
+
+        [HttpPost("CreateAutoshipHook")]
+        public async Task<ActionResult<CreateAutoshipHookResponse>> CreateAutoshipHook([FromBody] CreateAutoshipHookRequest request)
+        {
+            // This is an Example of a Before Hook
+            // The following lines add some text to the custom field of an autoship when it is created.
+            request.AutoshipInfo.Custom.Field1 = "This message was added from the CreateAutoshipHook";
+
+            // Execute the DirectScale callback method with the modified 'request' object that was passed into the controller as well as the one-time callbackToken
+            string callbackToken = HttpContext.Request.Headers["X-DirectScale-CallbackToken"];
+            CreateAutoshipHookResponse response = await _hookCallback.CallBack<CreateAutoshipHookRequest, CreateAutoshipHookResponse>(callbackToken, request);
+
+            return Ok(response);
+        }
+    }
+}
