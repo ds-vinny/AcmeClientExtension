b045c2e04d5f61f8f6fe91e9437df76309c9f6ba
Vinny <vinny@directscale.com>
2022-06-09 12:10:32 -0600

    change ws

--- ACMEClientExtension/Startup.cs
+++ ACMEClientExtension/Startup.cs
@@ -65,7 +65,6 @@ namespace ACMEClientExtension
                 options.AddMerchant<StripeMoneyInMerchantUsd>(9002, "Stripe Custom", "An example merchant", "USD");
                 options.AddMerchant<StripeMoneyInMerchantJpy>(9003, "Stripe Custom", "An example merchant", "JPY");
                 options.AddMerchant<MyCommissionMerchant>(9001, "MyCommissionMerchant", "An Example Commission Merchant", "USD");
-
                 options.AddMerchant<EWalletMoneyIn>(9100, "Ewallet Money In USD", "test", "USD");
                 options.AddMerchant<EWalletMoneyOut>(9101, "Ewallet Money Out USD", "test", "USD");
             });
