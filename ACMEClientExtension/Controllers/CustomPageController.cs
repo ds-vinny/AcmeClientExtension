2f899172a9450914ecd58f912f8f8a99f092da9a
Vinny <vinny@directscale.com>
2022-09-22 11:10:24 -0600

    Add Extension Logger to the HelloWorld page. Write notes about using the extension logger.

--- ACMEClientExtension/Controllers/CustomPageController.cs
+++ ACMEClientExtension/Controllers/CustomPageController.cs
@@ -15,13 +15,15 @@ namespace ACMEClientExtension.Controllers
 {
     public class CustomPageController : Controller
     {
-        private readonly ILogger<CustomPageController> _logger;
+        private readonly ILogger _logger;
+        private readonly ILoggerProvider _loggerProvider;
         private readonly IAssociateService _associateService;
         private readonly ICurrentUser _currentUser;
 
-        public CustomPageController(ILogger<CustomPageController> logger, IAssociateService associateService, ICurrentUser currentUser)
+        public CustomPageController(IAssociateService associateService, ICurrentUser currentUser, ILoggerProvider loggerProvider)
         {
-            _logger = logger;
+            _loggerProvider = loggerProvider ?? throw new ArgumentNullException(nameof(loggerProvider));
+            _logger = _loggerProvider.CreateLogger("CustomPageLogger");
             _associateService = associateService ?? throw new ArgumentNullException(nameof(associateService));
             _currentUser = currentUser ?? throw new ArgumentNullException(nameof(currentUser));
         }
@@ -45,8 +47,40 @@ namespace ACMEClientExtension.Controllers
             }
             model.Headers = headersKvps;
 
+            // See "MORE INFORMATION ABOUT LOGGING" comment below for more information about Extension Logging
+            _logger.LogInformation($"The Hello World Page was visited and displayed the following name: '{personsName}'.");
+
             return View(model);
         }
+
+        // **********************************************************
+        // ************* MORE INFORMATION ABOUT LOGGING *************
+        // **********************************************************
+        //
+        // **** | How to view the Log | ****
+        // - Execute the following query in the SQL Manager. https://<clientid>.corpadmin.directscale.com/Corporate/Reports/SqlViewer
+        //      SELECT * FROM ExtensionLog L
+        //      WHERE L.eventName = 'CustomPageLogger'
+        //
+        // **** | Example of the log record | ****
+        //  recordnumber	last_modified	        logLevel	eventId	eventName	        message
+        //  4383	        9/22/2022 7:33:43 AM	2	        0	    CustomPageLogger	The Hello World Page was visited and displayed the following name: 'Sam'.
+        //
+        // **** | ExtensionLog Table Schema | ****
+        // Column           DataType        Nullable
+        // recordnumber     int             true
+        // last_modified    datetime        true
+        // logLevel         int             true
+        // eventId          int             true
+        // eventName        varchar(25)	    false
+        // message          varchar(255)	true
+        // scope            varchar(255)	false
+        //
+        // **** | NOTES | ****
+        // - Messages longer than 255 Characters are not supported.
+        //      - If you need messages longer than 255 characters then you will need to split the string and call the log function multiple times.
+        // - Logging is asynchronous
+        //      - Logs may not be in exact chronilogical order because they are written using async operations.
 
         [ExtensionAuthorize] // This authenticates that the request is coming from DirectScale
         public async Task<IActionResult> SecuredHelloWorld([FromQuery] int associateId)
